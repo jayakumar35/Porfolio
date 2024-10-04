54d68a7af28ccf71dc35c196bd9f856ea96c170d
jayakumar35 <jayakumar53c@gmail.com>
2024-10-04 14:13:02 +0530

    madify commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Services from './Components/Services';
 import Project from './Components/Project';
 import Contact from './Components/Contact';
 import Frent from './Components/Frent';
-import Ourworkpage from './Components/Ourworkpage';
 
 function App() {
   return (
