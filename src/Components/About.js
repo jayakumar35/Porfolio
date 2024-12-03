312c7d1253a1ba2ddab945e1790da909e64c8c18
jayakumar35 <jayakumar53c@gmail.com>
2024-12-03 09:55:12 +0530

    modify commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,10 +3,6 @@ import './About.scss';
 import Aboutimage from '../image/profile.jpg'
 import Githupimage from '../image/github.png'
 import Linkedinimage from '../image/linkedin.png'
-import Instagramimage from '../image/instagram.png'
-import Xtwitterimage from '../image/twitter.png'
-import Youtubeimage from '../image/youtube-white-logo.png'
-import Facebookimage from '../image/facebook.png'
 import resume from '../image/cv.pdf'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -26,7 +22,7 @@ function About() {
         <Col sm={12} md={6} lg={6}>
           <div className="about-datails">
             <h1 >About Me</h1>
-            <p><b>Hello! I'm Jayakumar</b> web designer from TamilNadu,Tiruvannamala i have rich experience in web site desing and building
+            <p><b>Hello! I'm Jayakumar</b> web designer  i have rich experience in web site desing and building
               also i am good at wordpress i love to talk with you about our unique</p>
 
              <a download href={resume} > <button className="cv-button"  >Resume</button></a>
@@ -35,10 +31,6 @@ function About() {
             <div class="social-media">
               <a href="https://github.com/jayakumar35"><img src={Githupimage} alt="githup" /></a>
               <a href="https://www.linkedin.com/in/jayakumar3598/"><img src={Linkedinimage} alt="linkedin" /></a>
-              <a href="https://www.instagram.com/j_a_y_a_k_u_m_a_r_53/"><img src={Instagramimage} alt="instagram" /></a>
-              <a href="https://x.com/jayakumar3598"><img src={Xtwitterimage} alt="x" /></a>
-              <a href="https://www.youtube.com/@Jksquad53"><img src={Youtubeimage} alt="youtube" /></a>
-              <a href="https://www.facebook.com/profile.php?id=100012086977710"><img src={Facebookimage} alt="facebook" /></a>
             </div>
             <div className="controlled">
               <LeftTabsExample />
